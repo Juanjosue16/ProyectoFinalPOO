767239750cd9b08d7c770d5f208c21efe8cca758
Juanjosue16 <djjosue522@gmail.com>
2022-07-13 22:58:30 -0500

    Se agrega el proyecto

---
+++ src/transport/Transport.java
@@ -0,0 +1,17 @@
+
+
+package transport;
+
+
+
+
+public class Transport {
+
+    
+    public static void main(String[] args) {
+        
+        botones_dinamicos.main(args);
+        
+    }
+
+}
